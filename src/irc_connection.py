4f207436d85929000ee44a7c04a436ee4aac5638
nuritravis <163040079+nuritravis@users.noreply.github.com>
2026-01-19 22:18:31 -0600

    add send_msg func

--- src/irc_connection.py
+++ src/irc_connection.py
@@ -10,11 +10,11 @@ USERNAME = os.getenv("USERNAME")
 CHANNEL = str(input("Channel: "))  
 URI = "wss://irc-ws.chat.twitch.tv:443"    
 
-def msg_constructor(msg: str):
-    irc_msg = f'PRIVMSG #{CHANNEL} :{msg} \r\n'
+def construct_msg(msg: str) -> str:
+    irc_msg = f'PRIVMSG #{CHANNEL} :{msg}\r\n'
     return irc_msg
 
-async def receive(ws):
+async def receive_msg(ws: object):
     while True:
         message = await ws.recv()
 
@@ -22,37 +22,37 @@ async def receive(ws):
         for line in message.split("\r\n"):
             if not line:
                 continue
-            
-            parsed_msg = irc_parser.parse_line(line)
-            
-            if parsed_msg is not None and parsed_msg[1][1] == "PRIVMSG":
-                user = irc_parser.get_user(parsed_msg)
-                print(f"{user}: {parsed_msg[2]}")
-
 
             # respond to PING to stay connected
             if line.startswith("PING"):
                 await ws.send(line.replace("PING", "PONG") + "\r\n")
 
-async def send(ws):
-    pass
+            # parse and display incoming PRIVMSGs
+            parsed_msg = irc_parser.parse_line(line)
+            if parsed_msg is not None and parsed_msg[1][1] == "PRIVMSG":
+                user = irc_parser.get_user(parsed_msg)
+                print(f"{user}: {parsed_msg[2]}")
+
+async def send_msg(ws: object):
+    while True:
+        user_input = await asyncio.to_thread(input)
+        chat_msg = construct_msg(user_input)
+        await ws.send(chat_msg)
 
 async def main():
 
-    
     async with websockets.connect(URI) as ws:
         await ws.send("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership\r\n")
         await ws.send(f"PASS oauth:{OAUTH_TOKEN}\r\n")
         await ws.send(f"NICK {USERNAME}\r\n")
         await ws.send(f"JOIN #{CHANNEL}\r\n")
         print("Connected. Listening...\n")
+
+        recv_task = asyncio.create_task(receive_msg(ws))
+        send_task = asyncio.create_task(send_msg(ws))
+        await asyncio.gather(send_task, recv_task)
         
-
-        await receive(ws)
-        #send()
-
-
-
+        
 asyncio.run(main())
 
 
